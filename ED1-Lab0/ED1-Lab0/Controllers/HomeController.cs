a206a86a9dc5952f0ef44e2c95b3b679bd593be0
kliskis <karenliska@outlook.com>
2023-01-25 08:26:13 -0600

    Ejemplo 1

---
+++ ED1-Lab0/ED1-Lab0/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ED1_Lab0.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ED1_Lab0.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Persona persona1;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            persona1 = new Persona();
+            persona1.Edad = 18;
+            persona1.Nombre = "Alland";
+            persona1.Profesion = "Ingeniero";
+
+        }
+
+        public Persona Index()
+        {
+            return persona1;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ED1-Lab0/ED1-Lab0/Models/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace ED1_Lab0.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set;  }
+        public string Profesion { get; set; }
+    }
+}
