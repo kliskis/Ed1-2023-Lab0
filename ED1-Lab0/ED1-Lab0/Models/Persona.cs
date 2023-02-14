0e31b49e0a63aeb45ee78464a46f65b4f3a2f682
kliskis <karenliska@outlook.com>
2023-02-14 06:45:57 -0600

    Update Persona.cs
    Agregando propiedades

--- ED1-Lab0/ED1-Lab0/Models/Persona.cs
+++ ED1-Lab0/ED1-Lab0/Models/Persona.cs
@@ -2,8 +2,10 @@
 {
     public class Persona
     {
+        //Propiedades generales Persona
         public string Nombre { get; set; }
         public int Edad { get; set;  }
         public string Profesion { get; set; }
+        public string Direccion { get; set; }
     }
 }
