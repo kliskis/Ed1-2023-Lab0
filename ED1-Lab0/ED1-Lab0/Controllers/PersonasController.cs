a6bedcf8559493762dc06148dda2bedf1d6ad25a
kliskis <karenliska@outlook.com>
2023-02-14 06:00:33 -0600

    Ejemplo extendido controllers

--- ED1-Lab0/ED1-Lab0/Controllers/PersonasController.cs
+++ ED1-Lab0/ED1-Lab0/Controllers/PersonasController.cs
@@ -1,22 +1,12 @@
-﻿using ED1_Lab0.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ED1_Lab0.Controllers
 {
-    [Route("[controller]")]
     public class PersonasController : Controller
     {
-        [HttpGet]
-        [Route("ejemploPersona")]
-        public Persona ObtenerPersona()
+        public IActionResult Index()
         {
-            Persona ejemplo = new Persona()
-            {
-                Nombre = "Pedro",
-                Profesion = "Medico",
-                Edad = 48
-            };
-            return ejemplo;
+            return View();
         }
     }
 }
